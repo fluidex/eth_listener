52d13b1463968594d2027902e8351dca1d590e2a
lightsing <light.tsing@gmail.com>
2021-08-18 09:19:56 +0800

    remove useless RefCell from ConfirmedBlockStream

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@ use futures::Stream;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::future::Future;
-use std::ops::DerefMut;
-use std::cell::RefCell;
 
 pub mod events {
     include!(concat!(env!("OUT_DIR"), "/events.rs"));
@@ -25,11 +23,11 @@ pub enum ConfirmedBlockStreamError {
 
 pub struct ConfirmedBlockStream<'a, P: PubsubClient> {
     provider: &'a Provider<P>,
-    rx: RefCell<Pin<Box<SubscriptionStream<'a, P, Block<H256>>>>>,
-    last_confirmed_block: RefCell<u64>,
-    newest_block: RefCell<u64>,
+    rx: Pin<Box<SubscriptionStream<'a, P, Block<H256>>>>,
+    last_confirmed_block: u64,
+    newest_block: u64,
     n_confirmations: u64,
-    last_poll: RefCell<Option<Pin<Box<(dyn Future<Output=Result<Option<Block<H256>>, ProviderError>> + 'a)>>>>
+    last_poll: Option<Pin<Box<(dyn Future<Output=Result<Option<Block<H256>>, ProviderError>> + 'a)>>>
 }
 
 impl <'a, P: PubsubClient> ConfirmedBlockStream<'a, P> {
@@ -42,11 +40,11 @@ impl <'a, P: PubsubClient> ConfirmedBlockStream<'a, P> {
         let newest_block = provider.get_block_number().await?.as_u64();
         Ok(Self {
             provider,
-            rx: RefCell::new(Box::pin(rx)),
-            last_confirmed_block: RefCell::new(from),
-            newest_block: RefCell::new(newest_block),
+            rx: Box::pin(rx),
+            last_confirmed_block: from,
+            newest_block,
             n_confirmations,
-            last_poll: RefCell::new(None),
+            last_poll: None,
         })
     }
 }
@@ -56,11 +54,12 @@ impl <'a, P: PubsubClient> Stream for ConfirmedBlockStream<'a, P> {
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         // poll future if exist
-        if let Some(mut fut) = self.last_poll.take() {
+        let this = self.get_mut();
+        if let Some(mut fut) = this.last_poll.take() {
             if let Poll::Ready(poll_result) = fut.as_mut().poll(cx) {
                 let result = match poll_result {
                     Ok(block) => {
-                        *self.last_confirmed_block.borrow_mut() += 1;
+                        this.last_confirmed_block += 1;
                         Ok(block.unwrap())
                     },
                     Err(e) => {
@@ -69,26 +68,26 @@ impl <'a, P: PubsubClient> Stream for ConfirmedBlockStream<'a, P> {
                 };
                 return Poll::Ready(Some(result));
             }
-            self.last_poll.replace(Some(fut));
+            this.last_poll.replace(fut);
         }
 
         // assign future if there is remaining block
-        if *self.last_confirmed_block.borrow() < *self.newest_block.borrow() - self.n_confirmations {
-            let fut = Box::pin(self.provider.get_block(*self.last_confirmed_block.borrow() + 1));
-            self.last_poll.replace(Some(fut));
+        if this.last_confirmed_block < this.newest_block - this.n_confirmations {
+            let fut = Box::pin(this.provider.get_block(this.last_confirmed_block + 1));
+            this.last_poll.replace(fut);
             // immediately poll after create
-            return self.poll_next(cx);
+            return Pin::new(this).poll_next(cx);
         }
 
         // poll the stream for new block
-        let block = match futures_util::ready!(self.rx.borrow_mut().as_mut().poll_next(cx)) {
+        let block = match futures_util::ready!(this.rx.as_mut().poll_next(cx)) {
             Some(block) => block,
             // the stream is terminated
             None => return Poll::Ready(None),
         };
 
         // we got new block here, update
-        self.newest_block.replace(block.number.unwrap().as_u64());
-        return self.poll_next(cx);
+        this.newest_block = block.number.unwrap().as_u64();
+        return Pin::new(this).poll_next(cx);
     }
 }
