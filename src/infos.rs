9d3937890c8c49753ba88a8c46ec314e2cff12bb
lightsing <light.tsing@gmail.com>
2021-12-12 22:12:45 +0800

    add unit test on ContractInfos

--- src/infos.rs
+++ src/infos.rs
@@ -192,3 +192,56 @@ impl<M: Middleware> ContractInfos<M> {
         };
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use ethers::prelude::*;
+    use std::convert::TryFrom;
+    use std::str::FromStr;
+
+    use super::*;
+
+    const INFURA: &'static str = "https://goerli.infura.io/v3/71e500f0f56944fa80641312fdd9a6a4";
+    const CONTRACT_ADDRESS: &'static str = "0x1e8b07682E5ED8e7a666605a78B74cBdc7dC9455";
+
+    const ERC20_1: &'static str = "0x46490225a85ddfd9d79256f8c5393c0428121488";
+    const USER_PK_1: &'static str =
+        "5d182c51bcfe99583d7075a7a0c10d96bef82b8a059c4bf8c5f6e7124cf2bba3";
+
+    #[tokio::test]
+    async fn test_read() {
+        let provider = Arc::new(Provider::try_from(INFURA).unwrap());
+        let mut contract_info =
+            ContractInfos::new(provider, CONTRACT_ADDRESS.parse().unwrap()).await;
+
+        // read erc20
+        let address = contract_info.fetch_token_address(1).await.unwrap();
+        assert_eq!(Address::from_str(ERC20_1).unwrap(), address);
+
+        // read asset
+        let asset = contract_info.fetch_assets(1).await.unwrap();
+        assert_eq!("USDT", asset.id);
+        assert_eq!("USDT", asset.symbol);
+        assert_eq!("Tether USD (Fluidex Test)", asset.name);
+        assert_eq!(1, asset.chain_id);
+        assert_eq!(ERC20_1, asset.token_address.to_ascii_lowercase());
+        assert_eq!(1, asset.rollup_token_id);
+        assert_eq!(6, asset.prec_save);
+        assert_eq!(6, asset.prec_show);
+
+        // read token_id
+        let token_id = contract_info
+            .fetch_token_id(ERC20_1.parse().unwrap())
+            .await
+            .unwrap();
+        assert_eq!(1, token_id);
+
+        // read user_id
+        let pubkey = hex::decode(USER_PK_1).unwrap();
+        let user_id = contract_info
+            .fetch_user_id(pubkey.as_slice().try_into().unwrap())
+            .await
+            .unwrap();
+        assert_eq!(1, user_id);
+    }
+}
